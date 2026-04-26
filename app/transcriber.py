f4f3243ed075dd11e33aba8147975a3447f99c3b
Hisham <hisham.khashman@gmail.com>
2026-04-26 19:52:59 +0900

    feat: structure for transcribe function outlined

---
+++ app/transcriber.py
@@ -0,0 +1,18 @@
+#imports
+
+#import os
+
+
+
+#input variable input = "audio file path"
+
+#output output = transcript text (data/meetings/meetingtitledate.text not here just as a refference to where it will actually go)
+
+# funciton def trasncribe_audio  with the following actions:
+# 1 open audio file
+
+# 2 send to AI
+
+# 3 return transcript
+
+
