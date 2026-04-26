4d901230c78e62c0f516df12660a16811d9eb3dc
Hisham <hisham.khashman@gmail.com>
2026-04-26 17:46:04 +0900

    chore: added summarizer stucture

--- app/summarizer.py
+++ app/summarizer.py
@@ -4,5 +4,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# the ai client
+ai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+def summarize_meeting(transcript: str):
+    prompt = f"""
+you are Hisham jr., a sharp meeting assistant.
 
+Analyze the meeting transcript and return:
+
+1. Summary(short)
+2. Key Decisions
+3. Action Items
+4. Risks/ Concerns
+
+Be concise and structured.
+
+Transcript:
+{transcript}
+"""
+
+    response = ai_client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0.3,
+    )
+
+    return response.choices[0].message.content
