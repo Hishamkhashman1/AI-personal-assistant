0ca5c665756520b83db7595c4911401c3509e966
Hisham <hisham.khashman@gmail.com>
2026-04-28 01:51:40 +0900

    refactor: refactored job code to have the finally browser close inside the event loop

--- app/jobs.py
+++ app/jobs.py
@@ -4,23 +4,21 @@ def join_meeting_job(meeting_url: str, title: str):
     print(f"Job Started for: {title}")
     print(f"Meeting URL:{meeting_url}")
     browser = None
-    try:
-        with sync_playwright() as p:
-            browser = p.chromium.launch(
-                    headless=False,
-                    args=["--no-sandbox", "--disable-dev-shm-usage"],
-            )
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            headless=False,
+            args=["--no-sandbox", "--disable-dev-shm-usage"],
+            )   
+        try:
             page = browser.new_page()
-
             page.goto(meeting_url, wait_until="domcontentloaded", timeout=60_000)
             print("Page opened successfully")
-
             page.wait_for_timeout(10_000)
 
             print(f"Browser session finished for {title}")
-    finally:
-       if browser:
-        browser.close()
+        finally:
+            if browser:
+                browser.close()
 
     return {
         "title": title,
