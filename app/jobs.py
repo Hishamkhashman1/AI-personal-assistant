a51166a0c4beb5eaf65e77d2ab42170569a2453b
Hisham <hisham.khashman@gmail.com>
2026-04-28 01:26:03 +0900

    feat: added playwright to open browser

--- app/jobs.py
+++ app/jobs.py
@@ -1,16 +1,30 @@
-import time 
+from playwright.sync_api import sync_playwright
 
 def join_meeting_job(meeting_url: str, title: str):
     print(f"Job Started for: {title}")
     print(f"Meeting URL:{meeting_url}")
+    browser = None
+    try:
+        with sync_playwright() as p:
+            browser = p.chromium.launch(
+                    headless=False,
+                    args=["--no-sandbox", "--disable-dev-shm-usage"],
+            )
+            page = browser.new_page()
 
-    time.sleep(10)
+            page.goto(meeting_url, wait_until="domcontentloaded", timeout=60_000)
+            print("Page opened successfully")
 
-    print(f"Job for {title} Completed Succesfully")
+            page.wait_for_timeout(10_000)
+
+            print(f"Browser session finished for {title}")
+    finally:
+       if browser:
+        browser.close()
 
     return {
         "title": title,
         "meeting_url": meeting_url,
-        "status": "simulated_done"
+        "status": "browser_opened_and_closed",
     }
 
