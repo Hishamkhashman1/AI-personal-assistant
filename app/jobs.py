d45b9f4f6d4830f5030a8c8785db6a2018d38612
Hisham <hisham.khashman@gmail.com>
2026-04-27 19:08:38 +0900

    feat: added a shell for simulated job features to test

---
+++ app/jobs.py
@@ -0,0 +1,16 @@
+import time 
+
+def join_meeting_url(meeting_url: str, title: str):
+    print(f"Job Started for: {title}")
+    print(f"Meeting URL:{meeting_url}")
+
+    time.sleep(10)
+
+    print(f"Job for {title} Completed Succesfully")
+
+    return {
+        "title": title,
+        "meeting_url": meeting_url,
+        "status": "simulated_done"
+    }
+
